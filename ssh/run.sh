4ae1a49c07d84804505c7ba6a85fb5a8431e4592
Benjamin Kitt <bkitt@spscommerce.com>
2015-11-23 21:59:35 -0600

    Rename user variable in run script

--- ssh/run.sh
+++ ssh/run.sh
@@ -2,26 +2,26 @@
 
 if [ "${AUTHORIZED_KEYS}" != "**None**" ]; then
     echo "=> Found authorized keys"
-    : ${SSH_USERNAME:=user}
+    : ${SSH_USERNAME:=USER}
     : ${SSH_USERPASS:=$(dd if=/dev/urandom bs=1 count=15 | base64)}
-    useradd -ms /bin/bash -G sudo $SSH_USERNAME
-    echo -e "$SSH_USERPASS\n$SSH_USERPASS" | (passwd --quiet $SSH_USERNAME)
+    useradd -ms /bin/bash -G sudo $USER
+    echo -e "$SSH_USERPASS\n$SSH_USERPASS" | (passwd --quiet $USER)
     echo ssh user password: $SSH_USERPASS
-    mkdir -p /home/$SSH_USERNAME/.ssh
-    chmod 700 /home/$SSH_USERNAME/.ssh
-    chown $SSH_USERNAME:$SSH_USERNAME /home/$SSH_USERNAME/.ssh
-    touch /home/$SSH_USERNAME/.ssh/authorized_keys
-    chmod 600 /home/$SSH_USERNAME/.ssh/authorized_keys
-    chown $SSH_USERNAME:$SSH_USERNAME /home/$SSH_USERNAME/.ssh/authorized_keys
+    mkdir -p /home/$USER/.ssh
+    chmod 700 /home/$USER/.ssh
+    chown $USER:$USER /home/$USER/.ssh
+    touch /home/$USER/.ssh/authorized_keys
+    chmod 600 /home/$USER/.ssh/authorized_keys
+    chown $USER:$USER /home/$USER/.ssh/authorized_keys
     IFS=$'\n'
     arr=$(echo ${AUTHORIZED_KEYS} | tr "," "\n")
     for x in $arr
     do
         x=$(echo $x |sed -e 's/^ *//' -e 's/ *$//')
-        cat /home/$SSH_USERNAME/.ssh/authorized_keys | grep "$x" >/dev/null 2>&1
+        cat /home/$USER/.ssh/authorized_keys | grep "$x" >/dev/null 2>&1
         if [ $? -ne 0 ]; then
-            echo "=> Adding public key to /home/$SSH_USERNAME/.ssh/authorized_keys: $x"
-            echo "$x" >> /home/$SSH_USERNAME/.ssh/authorized_keys
+            echo "=> Adding public key to /home/$USER/.ssh/authorized_keys: $x"
+            echo "$x" >> /home/$USER/.ssh/authorized_keys
         fi
     done
 fi
